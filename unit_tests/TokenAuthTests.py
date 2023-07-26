f99e213ca33ca03b179e19ad516a59f36c2eba73
yildirimege <ege307@gmail.com>
2023-07-26 09:58:50 +0300

    Added token clearer service, also database now stores expiration time rather than time registered

--- unit_tests/TokenAuthTests.py
+++ unit_tests/TokenAuthTests.py
@@ -1,19 +1,37 @@
 import json
 import unittest
+import uuid
 from datetime import datetime, timedelta
 from unittest.mock import patch
-
+import os
+import sys
+import inspect
 from flask import Flask
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))  # Changing CWD to import
+                                                                                         # classes without creating a package.
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from database.database import Database
+from settings import Settings
 from controller import Controller
 
 
 class TokenAuthorizationTests(unittest.TestCase):
+    test_db = Database()
+    test_settings = Settings()
 
     def setUp(self):
         self.app = Flask(__name__)
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
-        self.controller = Controller(None, None)  # Replace None with actual instances of Settings and Database
+
+        self.database = Database()
+        self.settings = Settings()
+        self.controller = Controller(self.database, self.settings)
+
+
 
     def test_valid_token(self):
         with patch('controller.get_token_timestamp') as mock_get_token_timestamp:
